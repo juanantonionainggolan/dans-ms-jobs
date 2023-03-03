088db5865374d1822290ad3e14b00413e50eadd7
Juan Antonio Nainggolan <juanantonionainggolan@Juans-MacBook-Air.local>
2023-03-04 02:11:40 +0700

    Add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js"
 
@@ -15,6 +16,7 @@ try {
     console.log(error)
 }
 
+app.use(cors({ credentials:true, origin:'http://localhost:3000'}))
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
