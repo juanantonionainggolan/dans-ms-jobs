df06ad9dd3be7a39be86a687a26c9a8fa74589b0
Juan Antonio Nainggolan <juanantonionainggolan@Juans-MacBook-Air.local>
2023-03-04 05:58:30 +0700

    add get jobs query ability

--- controller/Jobs.js
+++ controller/Jobs.js
@@ -1,17 +1,33 @@
 import axios from "axios";
 
 export const getJobs = async (req, res) => {
+    const query = req.query;
+    const { description, city, full_time } = query
+    let paramArray = [];
+    if (description && description !== '')  {
+        paramArray.push(`description=${description}`)
+    };
+    if (city && city !== '')  {
+        paramArray.push(`city=${city}`)
+    };
+    if (full_time === 'true')  {
+        paramArray.push(`full_time=${full_time}`)
+    };
+    let queryString = (paramArray.length === 0) ? '' : '?'
+    queryString = queryString + paramArray.join('&');
+
     const options = {
         'method': 'get',
-        'url': 'http://dev3.dansmultipro.co.id/api/recruitment/positions.json',
+        'url': `http://dev3.dansmultipro.co.id/api/recruitment/positions.json${queryString}`,
         'headers': {
           'Content-Type': 'application/json'
         }
       };
+      console.log(options)
     
       try {
         const result = await axios(options);
-        console.log(result.data);
+        // console.log(result.data);
          return await res.send(result.data);
       } catch (e) {
            console.log(e);
